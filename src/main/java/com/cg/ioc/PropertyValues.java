ac776819b51a3031cddfe6ee47ef8b4a00aa6333
cgIIrw <chenguo_jay@163.com>
2018-07-08 17:05:54 +0800

    添加属性值

---
+++ src/main/java/com/cg/ioc/PropertyValues.java
@@ -0,0 +1,20 @@
+package com.cg.ioc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PropertyValues {
+    private final List<PropertyValue> propertyValueList = new ArrayList<>();
+
+    public PropertyValues() {
+
+    }
+
+    public void addPropertyValue(PropertyValue pv) {
+        this.propertyValueList.add(pv);
+    }
+
+    public List<PropertyValue> getPropertyValueList() {
+        return propertyValueList;
+    }
+}
