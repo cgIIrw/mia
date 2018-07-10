1d38c2cdef715967c81622b3455015915786d2e0
cgIIrw <chenguo_jay@163.com>
2018-07-10 18:57:49 +0800

    添加带解析expression的切点实现，添加advisor

---
+++ src/main/java/com/cg/aop/AspectJExpressionPointcutAdvisor.java
@@ -0,0 +1,24 @@
+package com.cg.aop;
+
+import org.aopalliance.aop.Advice;
+
+public class AspectJExpressionPointcutAdvisor implements PointcutAdvisor {
+
+    private AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+
+    private Advice advice;
+
+    public void setExpression(String expression) {
+        this.pointcut.setExpression(expression);
+    }
+
+    @Override
+    public Pointcut getPointcut() {
+        return pointcut;
+    }
+
+    @Override
+    public Advice geAdvice() {
+        return advice;
+    }
+}
