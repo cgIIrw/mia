ecc791fde475a4f0a21eaf482f7f9d2f467ec941
cgIIrw <chenguo_jay@163.com>
2018-07-10 09:52:06 +0800

    添加动态代理类

---
+++ src/main/java/com/cg/aop/JdkDynamicAopProxy.java
@@ -0,0 +1,28 @@
+package com.cg.aop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class JdkDynamicAopProxy implements AopProxy, InvocationHandler {
+
+    private  AdvisedSupport advised;
+
+    public JdkDynamicAopProxy(AdvisedSupport advised) {
+        this.advised = advised;
+    }
+
+    @Override
+    public Object getProxy() {
+        return Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] {advised.getClass()}, this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        MethodInterceptor methodInterceptor = advised.getMethodInterceptor();
+
+        return methodInterceptor.invoke(new ReflectiveMethodInvocation(advised.getTargetSource().getTarget(), method, args));
+    }
+}
